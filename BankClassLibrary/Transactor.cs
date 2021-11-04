a8a0be31018e23da8dff352dac5c76f354fdc3eb
James Brosnahan <jb172@zips.uakron.edu>
2021-11-04 17:35:03 -0400

    Code Challenge

---
+++ BankClassLibrary/Transactor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankClassLibrary
+{
+    public static class Transactor
+    {
+        public static bool Deposit(Account account, float amount)
+        {
+            account.balance += amount;
+            return true;
+        }
+
+        public static bool Withdrawal(Account account, float amount)
+        {
+            if(account.GetType().Equals(typeof(IndividualInvestmentAccount)) && amount > 500)
+            {
+                return false;
+            }
+            account.balance -= amount;
+            return true;
+        }
+
+        public static bool Transfer(Account SenderAccount, Account ReceiverAccount, float amount)
+        {
+            bool withdrawal = Transactor.Withdrawal(SenderAccount, amount);
+            if(withdrawal == false)
+            {
+                return false;
+            }
+            Transactor.Deposit(ReceiverAccount, amount);
+            return true;
+        }
+    }
+}
--- BankClassLibraryTests/TransactorTests.cs
+++ BankClassLibraryTests/TransactorTests.cs
@@ -15,21 +15,73 @@ namespace BankClassLibraryTests
             float expectedBalance = 300_000;
 
             Account account = new Account();
-
+            account.balance = startingBalance;
+            bool result = Transactor.Deposit(account, depositAmount);
+            Assert.IsTrue(result);
+            Assert.AreEqual(account.balance, expectedBalance);
         }
         [TestMethod]
         public void TestWithdrawal()
         {
+            float withdrawalAmount = 100_000;
+            float startingBalance = 200_000;
+            float expectedBalance = 100_000;
+
+            Account account = new Account();
+            account.balance = startingBalance;
+
+            bool result = Transactor.Withdrawal(account, withdrawalAmount);
+            Assert.AreEqual(account.balance, expectedBalance);
         }
 
         [TestMethod]
         public void Test500DollarWithdrawalLimitOnIndividualInvestmentAccounts()
         {
+            float withdrawalAmount = 100_000;
+            float startingBalance = 200_000;
+            float expectedBalance = 200_000;
 
+            IndividualInvestmentAccount account = new IndividualInvestmentAccount();
+            account.balance = startingBalance;
+
+            bool result = Transactor.Withdrawal(account, withdrawalAmount);
+            Assert.IsFalse(result);
+            Assert.AreEqual(account.balance, expectedBalance);
         }
         [TestMethod]
         public void TestTransfer()
         {
+            Account senderAccount = new Account();
+            Account receiverAccount = new Account();
+            float senderStartingBalance = 100_000;
+            float receiverStartingBalance = 100_000;
+            senderAccount.balance = senderStartingBalance;
+            receiverAccount.balance = receiverStartingBalance;
+            float transferAmount = 50_000;
+            float expectedSenderBalance = 50_000;
+            float expectedReceiverBalance = 150_000;
+            bool result = Transactor.Transfer(senderAccount, receiverAccount, transferAmount);
+            Assert.IsTrue(result);
+            Assert.AreEqual(senderAccount.balance, expectedSenderBalance);
+            Assert.AreEqual(receiverAccount.balance, expectedReceiverBalance);
+        }
+
+        [TestMethod]
+        public void Test500DollarWithdrawalLimitOnIndividualInvestmentTransfer()
+        {
+            IndividualInvestmentAccount senderAccount = new IndividualInvestmentAccount();
+            Account receiverAccount = new Account();
+            float senderStartingBalance = 100_000;
+            float receiverStartingBalance = 100_000;
+            senderAccount.balance = senderStartingBalance;
+            receiverAccount.balance = receiverStartingBalance;
+            float transferAmount = 50_000;
+            float expectedSenderBalance = 100_000;
+            float expectedReceiverBalance = 100_000;
+            bool result = Transactor.Transfer(senderAccount, receiverAccount, transferAmount);
+            Assert.IsFalse(result);
+            Assert.AreEqual(senderAccount.balance, expectedSenderBalance);
+            Assert.AreEqual(receiverAccount.balance, expectedReceiverBalance);
         }
 
     }
